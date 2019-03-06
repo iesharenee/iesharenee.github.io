0b8eb5755477b91c4fe7fdd67681f658f908f02c
Iesha Magallanes <imagalla@nd.edu>
2019-03-06 13:26:47 -0500

    Project 2 Update

---
+++ project 2/js/popup-template.js
@@ -0,0 +1,75 @@
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#clutch-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#clutch-modal").toggleClass("visible");
+        });
+}); 
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
+
+
+
+
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#break-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#break-modal").toggleClass("visible");
+        });
+}); 
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
+
+
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#gas-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#gas-modal").toggleClass("visible");
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
+
+
+
+
+
+
+
+//************************ Templates ************************
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#place-button-name-here').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#place-modal-name-here").toggleClass("visible");
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
